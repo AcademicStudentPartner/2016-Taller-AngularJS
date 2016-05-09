b12e559b977e3193296b85179445a4e85d7c5a56
maovares <mrodriguezovares@gmail.com>
2016-05-09 16:02:00 -0600

    Añadido servicio para request

--- common/services/request-service.js
+++ common/services/request-service.js
@@ -13,18 +13,19 @@ angular.module('ASPStore')
 .factory('requestService', function ($http) {
 
       return {
-            request: function (data, configs, callback) {
+            request: function (configs, data, callback) {
                   return $http(
                           {
-                                method: data.method,
-                                url: configs.url + data.params,
-                                data: data.data
+                                method: configs.method,
+                                url: configs.url + configs.params,
+                                data: data
                           })
                           .then(function(response) {
                                 callback(response.data);
 
-                          }, 
+                          },
                           function errorCallback(response) {
+                              console.log(response);
                                 callback({success: false, data: null});
                 });
             }
